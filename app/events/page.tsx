4abe9ceed681039d654cc6e602656ddf608b546a
John Ventura <jpventura925@gmail.com>
2023-11-29 12:25:48 -0500

    add tag filtering

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,32 +2,64 @@ import { Button } from "@/components/ui/button";
 import EventDisplay from "@/components/events/shared/EventDisplay";
 import createSupabaseServerClient from "@/utils/supabase/server";
 import EventCard from "@/components/events/events-public/EventCard";
+import TagFiltering from "@/components/events/client-components/TagFiltering";
+import { Suspense } from "react";
 
-export default async function Events() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
   const supabase = await createSupabaseServerClient();
-  const { data: events, error } = await supabase.from("events").select("*");
+  let { data: events, error } = await supabase.from("events").select("*");
+
+  if (query) {
+    const { data: tagIds, error: tagIdsError } = await supabase
+      .from("tags")
+      .select("id")
+      .eq("name", query);
+
+    const tagIdArray = tagIds?.map((tag) => tag.id) || [];
+    const { data: eventIds, error: eventIdsError } = await supabase
+      .from("event_tags")
+      .select("event_id")
+      .in("tag_id", tagIdArray);
+
+    const eventIdArray = eventIds?.map((event) => event.event_id) || [];
+    let { data, error } = await supabase
+      .from("events")
+      .select("*")
+      .in("id", eventIdArray);
+    events = data;
+  }
 
   return (
-    <main className="max-w-xl md:max-w-6xl xl:max-w-7xl m-auto">
+    <main className="max-w-xl m-auto">
       <div className="flex space-x-2 mb-2">
         <Button>New York, NY</Button>
         <Button>Date</Button>
       </div>
-      <div className="flex space-x-2 overflow-scroll scrollbar-hidden mb-2">
-        <Button variant={"secondary"}>Pokemon</Button>
-        <Button variant={"secondary"}>Sports</Button>
-        <Button variant={"secondary"}>Anime</Button>
-        <Button variant={"secondary"}>Trading Cards</Button>
-        <Button variant={"secondary"}>Futbol</Button>
-        <Button variant={"secondary"}>Jobs</Button>
-      </div>
+      <TagFiltering />
       <div className="my-4">
-        <h1 className="font-semibold text-2xl">Popular Events</h1>
+        {query ? (
+          <h1 className="font-semibold text-2xl">Popular {query} Events</h1>
+        ) : (
+          <h1 className="font-semibold text-2xl">Popular Events</h1>
+        )}
         <h1 className="font-bold text-gray-500">In New York, NY</h1>
       </div>
       {events && events.length > 0 ? (
         <div className="space-y-8">
-          <EventDisplay event={events[0]} />
+          <Suspense
+            fallback={
+              <div className="h-40 bg-white rounded-full">Loading...</div>
+            }
+          >
+            <EventDisplay event={events[0]} />
+          </Suspense>
           {events.splice(1).map((event) => (
             <EventCard
               redirectTo={`/events/${event.id}`}
---
+++ components/events/client-components/TagFiltering.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useState, useEffect } from "react";
+import { Tables } from "@/types/supabase";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+export default function TagFiltering() {
+  const supabase = createClient();
+  const [tags, setTags] = useState<Tables<"tags">[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const [activeTag, setActiveTag] = useState<string>("");
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      const { data, error } = await supabase.from("tags").select("*");
+      if (data) {
+        const dbTags: Tables<"tags">[] = data;
+        setTags(dbTags);
+      }
+      setLoading(false);
+    };
+    setActiveTag(searchParams.get("query") || "");
+    fetchTags();
+  }, []);
+
+  const handleClick = (tag_name: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (tag_name === activeTag) {
+      setActiveTag("");
+      params.delete("query");
+    } else if (tag_name) {
+      setActiveTag(tag_name);
+      params.set("query", tag_name);
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <div className="flex space-x-2 overflow-scroll scrollbar-hidden mb-2">
+      {loading && (
+        <>
+          <Skeleton className="w-full h-10 rounded-sm" />
+          <Skeleton className="w-full h-10 rounded-sm" />
+          <Skeleton className="w-full h-10 rounded-sm" />
+          <Skeleton className="w-full h-10 rounded-sm" />
+        </>
+      )}
+      {tags?.map((tag) => (
+        <Button
+          onClick={() => handleClick(tag.name)}
+          variant={tag.name === activeTag ? "default" : "secondary"}
+        >
+          {tag.name}
+        </Button>
+      ))}
+    </div>
+  );
+}
