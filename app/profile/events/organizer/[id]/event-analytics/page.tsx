8f467090ea6265e11638e424c7d93c27fc7e7f60
santilopez10 <santisoccer10@gmail.com>
2023-12-01 13:33:10 -0500

    event-analytics page

--- app/profile/events/organizer/[id]/event-analytics/page.tsx
+++ app/profile/events/organizer/[id]/event-analytics/page.tsx
@@ -1,3 +1,24 @@
-export default async function Page() {
-  return console.log("Hello World");
+import AnalyticsCard from "@/components/events/event-analytics/AnalyticsCard";
+import createSupabaseServerClient from "@/utils/supabase/server";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const supabase = await createSupabaseServerClient();
+  const { data: ticketsData, error } = await supabase
+    .from("tickets")
+    .select("*")
+    .eq("event_id", params.id);
+
+  return (
+    <main className="m-auto max-w-lg">
+      <div className="flex flex-col w-full space-y-4">
+        {ticketsData &&
+          ticketsData.map((ticket) => (
+            <AnalyticsCard
+              ticket={ticket}
+              sold={Math.round(ticket.quantity * 0.6)}
+            />
+          ))}
+      </div>
+    </main>
+  );
 }
