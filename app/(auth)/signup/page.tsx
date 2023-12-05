2874a861dba88b08eae3ba1c923c62b876b6317a
John Ventura <jpventura925@gmail.com>
2023-12-05 17:08:56 -0500

    change signup to include first and last name and allow same usernames

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -13,49 +13,52 @@ import {
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { signUp } from "@/lib/actions/auth";
-import InstagramIcon from "@/components/icons/InstagramIcon";
-import TwitterIcon from "@/components/icons/TwitterIcon";
 import * as z from "zod";
 import Link from "next/link";
 import GoogleIcon from "@/components/icons/GoogleIcon";
 
 const formSchema = z.object({
-  username: z.string().min(1, {
-    message: "Please enter a username",
-  }),
   email: z.string().email({
     message: "Please enter a valid email",
+  }),
+  confirmEmail: z.string().email({
+    message: "Please enter a confirm email",
+  }),
+  firstName: z.string().min(1, {
+    message: "Please enter your first name",
+  }),
+  lastName: z.string().min(1, {
+    message: "Please enter your last name",
   }),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters long",
   }),
-  instagram: z.string().optional(),
-  twitter: z.string().optional(),
 });
 
 export default function Page() {
-  const [addSocial, setAddSocial] = useState(false);
   const [emailExistsError, setEmailExistsError] = useState(false);
-  const [userExistsError, setUserExistsError] = useState(false);
+  const [emailsConfirmationError, setEmailConfirmationError] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
-      instagram: "",
-      twitter: "",
     },
   });
 
   const onSubmit = async (formData: z.infer<typeof formSchema>) => {
+    if (formData.email !== formData.confirmEmail) {
+      setEmailConfirmationError(true);
+      return;
+    }
+
     try {
       const response = await signUp(formData);
       if (response?.error) {
-        if (response.error.type === "username_taken") {
-          setUserExistsError(true);
-        } else if (response.error.type === "email_taken") {
+        if (response.error.type === "email_taken") {
           setEmailExistsError(true);
         }
       }
@@ -64,35 +67,41 @@ export default function Page() {
     }
   };
 
-  const handleClickSocials = () => {
-    if (addSocial) {
-      form.setValue("instagram", "");
-      form.setValue("twitter", "");
-    }
-    setAddSocial(!addSocial);
-  };
-
   return (
     <main className="px-4 mt-8 max-w-lg m-auto w-full">
       <h1 className="text-2xl text-center mb-4">Sign up</h1>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Username" {...field} />
-                </FormControl>
-                <div className="h-1">
-                  <FormMessage>
-                    {userExistsError ? "Username already exists" : ""}
-                  </FormMessage>
-                </div>
-              </FormItem>
-            )}
-          />
+          <div className="flex space-x-2 w-full">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem className="flex-grow">
+                  <FormControl>
+                    <Input placeholder="First Name" {...field} />
+                  </FormControl>
+                  <div className="h-1">
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem className="flex-grow">
+                  <FormControl>
+                    <Input placeholder="Last Name" {...field} />
+                  </FormControl>
+                  <div className="h-1">
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+          </div>
           <FormField
             control={form.control}
             name="email"
@@ -104,6 +113,22 @@ export default function Page() {
                 <div className="h-1">
                   <FormMessage>
                     {emailExistsError ? "Email already in use" : ""}
+                  </FormMessage>
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmEmail"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Confirm Email" {...field} />
+                </FormControl>
+                <div className="h-1">
+                  <FormMessage>
+                    {emailsConfirmationError ? "Emails don't match" : ""}
                   </FormMessage>
                 </div>
               </FormItem>
@@ -123,52 +148,6 @@ export default function Page() {
               </FormItem>
             )}
           />
-          {addSocial && (
-            <>
-              <FormField
-                control={form.control}
-                name="instagram"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <div className="flex items-center">
-                        <InstagramIcon />
-                        <Input placeholder="Instagram" {...field} />
-                      </div>
-                    </FormControl>
-                    <div className="h-1">
-                      <FormMessage />
-                    </div>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="twitter"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <div className="flex items-center">
-                        <TwitterIcon />
-                        <Input placeholder="Twitter" {...field} />
-                      </div>
-                    </FormControl>
-                    <div className="h-1">
-                      <FormMessage />
-                    </div>
-                  </FormItem>
-                )}
-              />
-            </>
-          )}
-          <Button
-            onClick={handleClickSocials}
-            className="rounded-full tracking-wide"
-            variant={"secondary"}
-            type="button"
-          >
-            {addSocial ? "Clear Socials" : "Add Socials"}
-          </Button>
           <Button className="w-full" type="submit">
             Sign Up
           </Button>
