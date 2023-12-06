ca64c3b2b37bd46c13bd07561172c8fbe056ced4
John Ventura <jpventura925@gmail.com>
2023-12-06 17:40:34 -0500

    add events a vendor has applied to to my events

--- app/profile/events/page.tsx
+++ app/profile/events/page.tsx
@@ -2,13 +2,16 @@ import { validateUser } from "@/lib/actions/auth";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import ListUserEvents from "./ListUserEvents";
+import ListAppliedEvents from "./ListAppliedEvents";
 import LoadingListUserEvents from "./LoadingListUserEvents";
+import { User } from "@supabase/supabase-js";
 
 export default async function Page() {
   const { data: userData } = await validateUser();
   if (!userData.user) {
     redirect("/account");
   }
+  const user: User = userData.user;
 
   return (
     <main className="w-full max-w-md m-auto">
@@ -16,7 +19,11 @@ export default async function Page() {
         <h1>My Events</h1>
       </div>
       <Suspense fallback={<LoadingListUserEvents />}>
-        <ListUserEvents userData={userData} />
+        <ListUserEvents user={user} />
+      </Suspense>
+      <h1 className="mt-10 text-xl">Applied to:</h1>
+      <Suspense fallback={<LoadingListUserEvents />}>
+        <ListAppliedEvents user={user} />
       </Suspense>
     </main>
   );
