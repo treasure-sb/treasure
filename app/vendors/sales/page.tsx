86ffe6043a507c2e1c2660d62d71c31c7fe9a522
santilopez10 <santisoccer10@gmail.com>
2023-12-28 20:55:33 -0500

    minor changes

--- app/vendors/sales/page.tsx
+++ app/vendors/sales/page.tsx
@@ -2,10 +2,17 @@ import Link from "next/link";
 import { validateUser, logoutUser } from "@/lib/actions/auth";
 import { Button } from "@/components/ui/button";
 import { User } from "@supabase/supabase-js";
+import createSupabaseServerClient from "@/utils/supabase/server";
 
 export default async function Page() {
   const { data: userData } = await validateUser();
   const user: User = userData.user as User;
+
+  const supabase = await createSupabaseServerClient();
+  const { data, error } = await supabase
+    .from("vendor_transactions")
+    .select("*")
+    .eq("vendor_id", user.id);
 
   return (
     <main className="m-auto max-w-lg">
@@ -13,14 +20,16 @@ export default async function Page() {
         <h1 className="text-2xl m-auto font-semibold text-center">
           Vendors Corner
         </h1>
-        <Link href="/vendors/sales" className="">
-          <Button className="w-full">Sales</Button>
-        </Link>
-        <Link href="/vendors/edit-payments">
-          <Button className="w-full" variant={"secondary"}>
-            Payment Methods
-          </Button>
-        </Link>
+        {data?.map((transaction) => (
+          <div className="flex flex-col border">
+            <div className="flex flex-col">
+              <p>Amount: ${transaction.amount}</p>
+              <p>Item Name: {transaction.item_name}</p>
+              <p>Method: {transaction.method}</p>
+              <p>Created At: {transaction.created_at}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
