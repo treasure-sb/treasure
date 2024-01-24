d57e5ef27cece65a158cc910e1a5b047e817b84b
santilopez10 <santisoccer10@gmail.com>
2024-01-23 23:26:48 -0500

    email on application

--- lib/actions/vendors/applications/index.ts
+++ lib/actions/vendors/applications/index.ts
@@ -3,15 +3,18 @@ import createSupabaseServerClient from "@/utils/supabase/server";
 import { VendorApplication } from "@/types/applications";
 import { sendVendorAppReceivedEmail } from "../../emails";
 
-const submitVendorApplication = async (application: VendorApplication) => {
+const submitVendorApplication = async (
+  application: VendorApplication,
+  organizer_id: string
+) => {
   const supabase = await createSupabaseServerClient();
   const { error } = await supabase.from("event_vendors").insert([application]);
-  const { data: vendorData } = await supabase
+  const { data: hostData } = await supabase
     .from("profiles")
     .select("email")
-    .eq("id", application.vendor_id)
+    .eq("id", organizer_id)
     .single();
-  await sendVendorAppReceivedEmail(vendorData?.email);
+  await sendVendorAppReceivedEmail(hostData?.email);
 
   return { error };
 };
