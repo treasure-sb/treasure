8c875f3cc270575c26670512bffefe6c5d058f22
John Ventura <jpventura925@gmail.com>
2024-02-09 04:28:51 -0500

    remove landing animation

--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -21,17 +21,7 @@ export default function Hero() {
     const isColoredYellow = word === "Love";
     const isSpace = word === " ";
     return (
-      <motion.span
-        key={index}
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: index * 0.03,
-          duration: 0.65,
-          ease: "easeInOut",
-        }}
-        className={isSpace ? "" : "inline-block"}
-      >
+      <motion.span key={index} className={isSpace ? "" : "inline-block"}>
         {isColoredGreen ? (
           <span className="bg-gradient-to-r from-primary to-green-200 text-transparent bg-clip-text">
             {word}
@@ -53,17 +43,7 @@ export default function Hero() {
   const subheading = subheadingText.split(/(\s+)/).map((word, index) => {
     const isSpace = word === " ";
     return (
-      <motion.span
-        key={index}
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 2.25 + index * 0.03,
-          duration: 0.75,
-          ease: "easeInOut",
-        }}
-        className={isSpace ? "" : "inline-block"}
-      >
+      <motion.span key={index} className={isSpace ? "" : "inline-block"}>
         {word}
       </motion.span>
     );
@@ -73,32 +53,18 @@ export default function Hero() {
     <motion.section
       style={{ opacity }}
       ref={targetRef}
-      className="h-screen pt-10 md:pt-28 w-full before:z-0 before:bg-[radial-gradient(circle_farthest-side_at_calc(100px)_calc(100px),_var(--primary-hero)_0%,_transparent_100%)] before:pointer-events-none before:inset-0 before:fixed before:opacity-20"
+      className="h-screen pt-10 md:pt-28 w-full before:z-0 before:bg-[radial-gradient(circle_farthest-side_at_calc(100px)_calc(100px),_var(--primary-hero)_0%,_transparent_100%)] before:pointer-events-none before:inset-0 before:fixed before:opacity-10"
     >
       <motion.div style={{ scale }}>
         <div className="relative z-10">
-          <motion.p
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.75, ease: "easeInOut" }}
-            className="z-10 relative max-w-6xl text-[2.4rem] font-bold text-center m-auto leading-[1.2] md:leading-[1.3] md:text-7xl md:max-w-4xl"
-          >
+          <motion.p className="z-10 relative max-w-6xl text-[2.4rem] font-bold text-center m-auto leading-[1.2] md:leading-[1.3] md:text-7xl md:max-w-4xl">
             {heading}
           </motion.p>
           <p className="text-center text-md md:text-lg my-4">{subheading}</p>
           <div className="w-full flex justify-center items-center">
             <Link className="mt-2 md:mt-6" href="/events">
               <Button className="rounded-full" asChild>
-                <motion.button
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{
-                    delay: 1.45,
-                    duration: 0.75,
-                    ease: "easeInOut",
-                  }}
-                  className="text-black w-40 md:w-52 md:py-8 md:text-xl block font-semibold rounded-full"
-                >
+                <motion.button className="text-black w-40 md:w-52 md:py-8 md:text-xl block font-semibold rounded-full">
                   Browse Events
                 </motion.button>
               </Button>
