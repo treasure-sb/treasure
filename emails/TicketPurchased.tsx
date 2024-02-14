0a931b185d4622c5c8e8f5e8400cad5b01b78744
John Ventura <jpventura925@gmail.com>
2024-02-14 17:43:50 -0500

    update ticket purchased email

--- emails/TicketPurchased.tsx
+++ emails/TicketPurchased.tsx
@@ -6,7 +6,6 @@ import {
   Container,
   Heading,
   Hr,
-  Head,
   Html,
   Section,
   Text,
@@ -20,11 +19,25 @@ import {
 export interface TicketPurchasedProps {
   eventName: string;
   posterUrl: string;
+  ticketType: string;
+  quantity: number;
+  location: string;
+  date: string;
+  guestName: string;
+  totalPrice: string;
+  eventInfo: string;
 }
 
 export default function TicketPurchased({
   eventName = "Garden State Card Show",
   posterUrl = "https://qkdlfshzugzeqlznyqfv.supabase.co/storage/v1/object/public/posters/posters1701495337087",
+  ticketType = "General Admission",
+  quantity = 1,
+  location = "8868 1st St, Los Angeles, CA 90048 United States of America",
+  date = "Sat, March 3",
+  guestName = "John Ventura",
+  totalPrice = "$3.00",
+  eventInfo = "Round 2 was the best one yet of our shows! They just get bigger and bigger and round 3 will be no exception. 3/3/24 will have free public street parking and food set up in the back of the show which includes bagel with cream cheese, butter, peanut butter and hot dogs for lunch at an affordable rate. We will have over 60 vendors showcasing the very best of their products including Pokemon, plush, one piece, yugioh and many more! Best place to spend your tax refund! The first show was inspired by corocoro mew and our tee shirt design was made and finalized. Round 3 is inspired by the world renowned classic game",
 }: TicketPurchasedProps) {
   return (
     <Html>
@@ -33,7 +46,9 @@ export default function TicketPurchased({
           <Container className="bg-background border-2 max-w-lg px-10">
             <Header />
             <Section>
-              <Heading className="font-normal">Great choice, John</Heading>
+              <Heading className="font-normal">
+                Great choice, {guestName.split(" ")[0]}
+              </Heading>
               <Text>
                 You're going to <strong>{eventName}</strong>
               </Text>
@@ -68,35 +83,33 @@ export default function TicketPurchased({
                 <Column className="text-left text-foreground/40">
                   Ticket Type
                 </Column>
-                <Column className="text-right">General Admission</Column>
+                <Column className="text-right">{ticketType}</Column>
               </Row>
               <Hr className="border-foreground/40 my-6" />
               <Row>
                 <Column className="text-left text-foreground/40">
                   Quantity
                 </Column>
-                <Column className="text-right">1</Column>
+                <Column className="text-right">{quantity}</Column>
               </Row>
               <Hr className="border-foreground/40 my-6" />
               <Row>
                 <Column className="text-left text-foreground/40">
                   Location
                 </Column>
-                <Column className="text-right">Elks Lodge Queens</Column>
+                <Column className="text-right w-60">{location}</Column>
               </Row>
               <Hr className="border-foreground/40 my-6" />
               <Row>
-                <Column className="text-left text-foreground/40">
-                  Date & Time
-                </Column>
-                <Column className="text-right">Sat, March 3</Column>
+                <Column className="text-left text-foreground/40">Date</Column>
+                <Column className="text-right">{date}</Column>
               </Row>
               <Hr className="border-foreground/40 my-6" />
               <Row className="mb-6">
                 <Column className="text-left text-foreground/40">
                   Total Price
                 </Column>
-                <Column className="text-right">$3.00</Column>
+                <Column className="text-right">{totalPrice}</Column>
               </Row>
             </Section>
             <Section className="mb-6 border-solid border-[1px] rounded-[2.5rem] border-foreground/40 p-4 w-full text-center text-lg">
@@ -104,24 +117,7 @@ export default function TicketPurchased({
                 Event Information
               </Heading>
               <Hr className="border-foreground/40 my-6" />
-              <Text className="text-left text-foreground/40">
-                Round 2 was the best one yet of our shows! They just get bigger
-                and bigger and round 3 will be no exception. 3/3/24 will have
-                free public street parking and food set up in the back of the
-                show which includes bagel with cream cheese, butter, peanut
-                butter and hot dogs for lunch at an affordable rate. We will
-                have over 60 vendors showcasing the very best of their products
-                including Pokemon, plush, one piece, yugioh and many more! Best
-                place to spend your tax refund! The first show was inspired by
-                corocoro mew and our tee shirt design was made and finalized.
-                Round 3 is inspired by the world renowned classic game boy game
-                where the Pokemon are battling each other. Choose your game boy
-                color tee theme! We will have these shirts for sale with a $200
-                credit giveaway until 5/1/24!It was non stop show of giveaways
-                from the very best vendors and from the customers too! Thank you
-                to everyone who made this a moment to remember. Round 3
-                giveaways will be announced shortly!
-              </Text>
+              <Text className="text-left text-foreground/40">{eventInfo}</Text>
             </Section>
             <Section className="mb-6 border-solid border-[1px] rounded-[2.5rem] border-foreground/40 p-4 w-full text-center text-lg">
               <Heading as="h2" className="font-normal text-left">
