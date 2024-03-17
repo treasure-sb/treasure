4781368d6f65159a79bfeac1abd6aa868c8ce327
santilopez10 <santisoccer10@gmail.com>
2024-03-16 22:08:44 -0400

    landing page idea

--- components/landing-page/features/Features.tsx
+++ components/landing-page/features/Features.tsx
@@ -6,7 +6,7 @@ import LockIn from "./LockIn";
 
 export default function Features() {
   return (
-    <section className="p-4">
+    <section>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-4 w-full m-auto ">
         <Preview />
         <Book />
