0ac75f26f7fdc55c45ff28651284f3fa802626b2
santilopez10 <santisoccer10@gmail.com>
2024-03-24 23:01:25 -0400

    hamburger

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/shared/header/Header";
 import LoggedInHeader from "@/components/shared/header/LoggedInHeader";
 import { validateUser } from "@/lib/actions/auth";
+import createSupabaseServerClient from "@/utils/supabase/server";
+import { getProfile } from "@/lib/helpers/profiles";
 
 export default async function MainLayout({
   children,
@@ -11,10 +13,20 @@ export default async function MainLayout({
     data: { user },
   } = await validateUser();
 
+  const supabase = await createSupabaseServerClient();
+  const { profile } = await getProfile(user?.id);
+  const {
+    data: { publicUrl },
+  } = await supabase.storage.from("avatars").getPublicUrl(profile.avatar_url);
+
   return (
     <div className="p-6 px-4">
       <div className="relative z-10">
-        {user ? <LoggedInHeader user={user} /> : <Header />}
+        {user ? (
+          <LoggedInHeader profile={profile} publicUrl={publicUrl} />
+        ) : (
+          <Header />
+        )}
       </div>
       {children}
     </div>
