f8ed55f5120c5af11a2716a62f7cddacb4fd1866
John Ventura <jpventura925@gmail.com>
2024-05-15 17:00:02 -0400

    remove old header code

--- app/(dashboards)/components/mobile/MobileHeader.tsx
+++ app/(dashboards)/components/mobile/MobileHeader.tsx
@@ -10,10 +10,6 @@ export default function MobileHeader() {
   const isVendor = pathname.startsWith("/vendor");
 
   return (
-    // <div className="flex justify-between items-center md:hidden sticky p-4 top-0 bg-background z-50">
-    //   <Menu type={isVendor ? "vendor" : "host"} />
-    //   <HeaderAvatar />
-    // </div>
     <div className="z-50 block md:hidden relative">
       <HeaderMotion>
         <Menu type={isVendor ? "vendor" : "host"} />
