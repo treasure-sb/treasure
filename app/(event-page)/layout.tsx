dae134ca6db7ddca3621d94d9be57dd15c4411ad
John Ventura <jpventura925@gmail.com>
2024-06-26 15:02:10 -0400

    fix scroll to top of event page

--- app/(event-page)/layout.tsx
+++ app/(event-page)/layout.tsx
@@ -1,6 +1,7 @@
 import NonLoggedHeader from "@/components/shared/header/NonLoggedHeader";
 import LoggedInHeader from "@/components/shared/header/LoggedInHeader";
 import { validateUser } from "@/lib/actions/auth";
+import ScrollToTop from "@/components/shared/ScrollToTop";
 
 export default async function EventPageLayout({
   children,
@@ -12,15 +13,18 @@ export default async function EventPageLayout({
   } = await validateUser();
 
   return (
-    <div className="px-4 sm:px-8">
-      <div className="relative z-50">
-        {user ? (
-          <LoggedInHeader user={user} isEventPage={true} />
-        ) : (
-          <NonLoggedHeader isEventPage={true} />
-        )}
+    <>
+      <ScrollToTop />
+      <div className="px-4 sm:px-8">
+        <div className="relative z-50">
+          {user ? (
+            <LoggedInHeader user={user} isEventPage={true} />
+          ) : (
+            <NonLoggedHeader isEventPage={true} />
+          )}
+        </div>
+        <div className="pt-16 md:pt-20">{children}</div>
       </div>
-      <div className="pt-16 md:pt-20">{children}</div>
-    </div>
+    </>
   );
 }
