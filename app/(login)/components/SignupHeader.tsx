c920c6f6923701af72afcd885fe9fc4b64805be0
John Ventura <jpventura925@gmail.com>
2024-07-01 18:30:23 -0400

    fix import error

--- app/(login)/components/SignupHeader.tsx
+++ app/(login)/components/SignupHeader.tsx
@@ -3,7 +3,7 @@ import Logo from "@/components/icons/Logo";
 
 export default function SignupHeader() {
   return (
-    <header className="md:max-w-6xl xl:max-w-7xl m-auto w-full z-10">
+    <header className="md:max-w-6xl xl:max-w-7xl m-auto w-full z-20">
       <Link
         href="/home"
         className="font-bold text-3xl flex items-center justify-start space-x-1"
