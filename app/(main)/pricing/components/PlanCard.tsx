cdcfca018bfd5bac432f5bb09dcf4a0e51af5128
nicfigu <nicfigu@gmail.com>
2024-07-12 18:28:48 -0400

    pricing page reworked

--- app/(main)/pricing/components/PlanCard.tsx
+++ app/(main)/pricing/components/PlanCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { CheckCircle } from "lucide-react";
 interface PlanCardProps {
     name: string;
     description: string;
@@ -7,6 +8,7 @@ interface PlanCardProps {
     features: string[];
     color: string;
     recommended?: boolean;
+    percentFee: string
 }
 
 export default function PlanCard({
@@ -16,38 +18,64 @@ export default function PlanCard({
     features,
     color,
     btnText = 'Choose Plan',
-    recommended = false
+    recommended = false,
+    percentFee
+
 
 }: PlanCardProps) {
-    const backgroundColor = color
-    const length = features.length == 3 ? 41.5 : 8- ((features.length * 1.5))
+  
+    const length = 8- ((features.length * 1.5))
     return (
-    <div style ={{backgroundColor: color }}className="flex min-h-[428px] w-[320px] flex-col rounded-3xl p-8">
-      <div className="ml-4">
-          {recommended && <div className="self-center h-7"><Button className="rounded-3xl relative -right-20 -top-4" size="sm" style={{backgroundColor: "black", color: "black" }}>
+      <>{color === "black" ?<><div className="bg-black flex min-h-[428px] md:w-96 w-full flex-col rounded-3xl p-8">
+      <div className="ml-4 flex flex-col min-h-[428px] justify-between h-full">
+          {recommended && <div className="self-center h-7"><Button className="rounded-3xl relative -right-20 -top-4 text-black bg-black" size="sm">
               <text className="text-xs text-[#73D08D] font-bold">MOST POPULAR</text>
           </Button></div>}
-          <h2 className='mb-5 text-4xl font-bold text-left'  style={{color:backgroundColor === "black" ? "#73D08D" : "black"}}>
-            {price ? <><text className="font-bold text-4xl" style={{color:backgroundColor === "black" ? "#73D08D" : "black"}}>${price}</text><text className=" text-lg font-normal" style={{color:backgroundColor === "black" ? "white" : "black"}}> /month</text></>  
+          <h2 className='mb-5 text-4xl font-bold text-left text-[#73D08D]'  >
+            {price ? <><text className="font-bold text-4xl text-[#73D08D]" >${price}</text><text className=" text-lg font- text-white"> /month</text></>  
             : "Free"}
           </h2>
-          <div className="mb-2 flex items-end text-2xl font-medium" style={{color:backgroundColor === "black" ? "#73D08D" : "black"}}>{name} </div>
-          <p className="mb-5 text-left" style={{color:backgroundColor === "black" ? "white" : "black"}}>{description}</p>
-          <ul className="mb-10 flex flex-col gap-y-2">
+          <div className="mb-2 flex items-end text-2xl font-medium text-[#73D08D]">{name} </div>
+          <p className="mb-5 text-left text-white">{description}</p>
+          <div className="mb-10 flex flex-col gap-y-2">
               {features.map((feature) => (
-                  <li className="flex items-center mb-2" key={feature}>
-                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10C19.9936 4.47982 15.5202 0.00642897 10 0Z" fill="#73D08D"/>
-                          <path d="M15.7724 6.83337L10.0683 14.5742C9.93221 14.7547 9.72936 14.873 9.50527 14.9024C9.28118 14.9319 9.05465 14.8701 8.87659 14.7309L4.80325 11.4742C4.44381 11.1866 4.3856 10.662 4.67325 10.3025C4.9609 9.94309 5.48547 9.88489 5.84492 10.1725L9.24159 12.89L14.4308 5.84754C14.6009 5.5922 14.8976 5.45103 15.2031 5.48007C15.5086 5.50912 15.7734 5.70368 15.8923 5.98652C16.0113 6.26935 15.9653 6.59469 15.7724 6.83337Z" fill="#0D0F0E"/>
-                      </svg>
-                      <text className="ml-3" style={{color:backgroundColor === "black" ? "white" : "black"}}>{feature}</text>
-                  </li>
+                  <div className="flex items-center gap-2 w-full mb-2" key={feature}>
+                      <CheckCircle size={20} className="w-1/10" />
+                      <p className="ml-3 overflow-auto w-4/5 text-left text-white">{feature}{(feature.includes("Mobile")) && <span className="text-left text-[#808080] text-sm italic font-thin">  Beta</span>}</p>
+                      
+                  </div>
               ))}
-          </ul>
-          <p className="text-left mb-8" style={{color:backgroundColor === "black" ? "white" : "black"}}>4% Transaction Fees**</p>
-          <Button className="rounded-3xl sm:relative top-36" size="lg" style={{backgroundColor:backgroundColor === "black" ? "73D08D" : "black", color: backgroundColor === "black" ? "black" : "white", top:length }}>
+          </div>
+          <p className="text-left mb-8 text-white" >{percentFee}% Transaction Fees</p>
+          <Button className={`rounded-3xl relative bottom-3 bg-[#73D08D] text-black`} size="lg">
               <text className="px-4">{btnText}</text>
           </Button>
       </div>
-    </div>)
+    </div></> : <><div className="flex min-h-[428px] shadow-2xl shadow-[#78df95] md:w-96 full flex-col rounded-3xl p-8 md:relative bottom-6 bg-[#73D08D] ">
+      <div className="ml-4 flex flex-col min-h-[428px] justify-between h-full">
+          {recommended && <div className="self-center h-7"><Button className="rounded-3xl relative -right-20 -top-4 text-black bg-black" size="sm">
+              <text className="text-xs text-[#73D08D] font-bold">MOST POPULAR</text>
+          </Button></div>}
+          <h2 className='mb-5 text-4xl font-bold text-left text-black'>
+            {price ? <><text className="font-bold text-4xl text-black">${price}</text><text className=" text-lg font-normal text-black"> /month</text></>  
+            : "Free"}
+          </h2>
+          <div className="mb-2 flex items-end text-2xl font-medium text-black" >{name} </div>
+          <p className="mb-5 text-left text-black" >{description}</p>
+          <div className="mb-10 flex flex-col gap-y-2">
+              {features.map((feature) => (
+                  <div className="flex items-center gap-2 w-full mb-2" key={feature}>
+                      <CheckCircle size={20} className="w-1/10 text-black" />
+                      <p className="ml-3 overflow-auto w-4/5 text-left text-black" >{feature}{(feature.includes("Mobile")) && <span className="text-left text-[#808080] text-sm italic font-thin">  Beta</span>}</p>
+                      
+                  </div>
+              ))}
+          </div>
+          <p className="text-left mb-8 text-black" >{percentFee}% Transaction Fees</p>
+          <Button className={`rounded-3xl relative bottom-3 bg-black text-white`} size="lg">
+              <text className="px-4">{btnText}</text>
+          </Button>
+      </div>
+    </div></>}</>
+    )
 }
--- app/(main)/pricing/page.tsx
+++ app/(main)/pricing/page.tsx
@@ -3,81 +3,85 @@ import PlanCard from "./components/PlanCard";
 import ComparisonRow from "./components/ComparisonRow";
 
 export default function Page() {
+  const unicode = "&#65291"
     return (
-      <main className="">
+      <div className="">
         <div className="flex flex-col items-center">
-          <div className="mb-2 mt-0 text-center">
+          <div className="mb-2 mt-0 text-center mx-auto">
             <div className="w-full flex justify-center">
               <h1 className="sm:mb-8 text-[2.5rem] font-bold text-white">Start <span className="text-[#73D08D]">supercharging</span> your<br className="hidden sm:block"/> event management & growth today</h1> 
             </div>
             
-            <div className="flex sm:flex-row p-10 flex-col">
+            <div className="flex md:flex-row justify-center flex-col mx-auto my-8 gap-6 md:gap-0 w-full ">
+              
               <PlanCard
                 color="black"
                 name="Basic"
                 description="A plan with event basics. Recommended for small events"
-                features={["Unlimited Events", "Unlimited Applications", "Mobile Check-In App"]}
+                features={["Unlimited Events, Tickets and Vendor Applications", "One Admin Profile", "Weekly Payouts", "24/7 Customer Support", "Mobile Check-in App Access"]}
+                percentFee= "4"
               />
               
-              <div className="sm:relative sm:bottom-8 sm:shadow-2xl shadow-[#78df95] rounded-3xl">
+              
                   <PlanCard
                     color="#73D08D"
-                    name="Starter"
-                    description="Unlock data analytics and CRM tools to grow your events"
-                    features={["Unlimited Events", "Unlimited Applications", "Mobile Check-In App", "Attendee & Vendor CRM"]}
-                    price="20"
+                    name="Pro"
+                    description="The plan for most events. Made for small teams, businesses, and organizations."
+                    features={["Everything included in the Basic plan", "Embedded Checkout", "5 Admin/Staff Accounts", "Daily Payout"]}
+                    price="35"
                     recommended = {true}
+                    percentFee= "2"
                   />
-              </div>
-
               
+
               
               <PlanCard
                 color="black"
-                name="Pro"
-                description="Advanced tools to take your events to the next level"
-                features={["Unlimited Events", "Unlimited Applications", "Mobile Check-In App", "Full Analytics Suite"]}
-                price="40"
+                name="Enterprise"
+                description= "Our most coveted plan, including the best customer service, tailored features, and 24/7 support"
+                features={[`Everything included in the Pro plan`, "Dedicated Account Manager", "Full Analytics Suite", "Event Financing", "Tailored Features"]}
+                price=" --"
+                percentFee= "2"
+                btnText="Contact Us for Pricing"
               />
             </div>
               
           </div>
-          <p className="text-white italic text-xs sm:relative right-72 text-center">**Standard credit card and ACH fees not included.</p>
         </div>
-        <div className="hidden min-[1165px]:block scale-90">
+        <div className="hidden min-[1165px]:block scale-90" >
           <div className="items-center flex w-[100%]">
             <h1 className="mb-8 text-[2.5rem] font-bold text-white text-center w-[100%]">Compare Features by Plan.</h1>
           </div>
           <div className=" w-[100%]">
-            <div className="flex flex-row w-[100%] border-b-2 border-[#73D08D] pb-4">
+            <div className="flex flex-row w-[100%] border-b-2 border-[#73D08D] gap-4 pb-6">
               {/*Column Titles*/}
-              <div className="items-center w-[25%] mr-8 mt-3">
-                <span className="text-2xl font-bold mr-8 text-[#73D08D] ml-8">Compare Plans</span>
-                <div className="mt-10 ml-8">
-                <span className=" text-white text-sm ">Choose your Treasure plan according to your eventing needs</span>
-                </div>
+              <div className="items-center w-[25%] pl-8 flex flex-col gap-4 justify-end pb-2">
+                <h2 className="text-2xl font-bold text-[#73D08D] text-left w-full">Compare Plans</h2>
+                
+                <p className=" text-white text-sm">Choose your Treasure plan according to your eventing needs</p>
+                
               </div>
-              <div className="flex flex-col items-center w-1/4 mx-1">
+              <div className="flex flex-col items-center w-1/4">
                 <div className="text-4xl font-bold text-[#73D08D] text-center">
                   Basic <span className="text-[#858BA0] text-xs">free</span>
                 </div>
-                <div className="mt-8">
+                <div className="mt-6">
                   <Button size="lg" className="mt-2 px-16"><span className="mx-2">Choose This Plan</span></Button>
                 </div>
               </div>
-              <div className="flex flex-col items-center w-1/4 mx-1">
+              <div className="flex flex-col items-center w-1/4">
                 <div className="text-4xl font-bold text-[#73D08D] text-center">
-                  Starter <span className="text-[#858BA0] text-xs">$20/month</span>
+                  Pro <span className="text-[#858BA0] text-xs">$35/month</span>
                 </div>
-                <div className="mt-8">
+                <div className="mt-6">
                   <Button size="lg" className="mt-2 px-16"><span className="mx-2">Choose This Plan</span></Button>
                 </div>
               </div>
-              <div className="flex flex-col items-center w-1/4 mx-1">
+              <div className="flex flex-col items-center w-1/4">
                 <div className="text-4xl font-bold text-[#73D08D] text-center">
-                  Pro <span className="text-[#858BA0] text-xs">$40/month</span>
+                  Enterprise <span className="text-[#858BA0] text-xs">$ - -/month</span>
                 </div>
-                <div className="mt-8">
+                <div className="mt-6">
                   <Button size="lg" className="mt-2 px-16"><span className="mx-2">Choose This Plan</span></Button>
                 </div>
               </div>
@@ -86,19 +90,25 @@ export default function Page() {
           </div>
           {/*Rows*/}
           <ComparisonRow name="Unlimited Events" col1={true} col2={true} col3={true} />
-          <ComparisonRow name="Mobile Check-In App" col1={true} col2={true} col3={true} isGreen={true}/>
-          <ComparisonRow name="Full Vendor CRM Suite" col3={true} />
-          <ComparisonRow name="Dedicated URL per event" col1={true} col2={true} col3={true} isGreen={true}/>
-          <ComparisonRow name="Social Media Sharing" col2={true} col3={true} />
-          <ComparisonRow name="Event Highlights" col2={true} col3={true} isGreen={true}/>
-          <ComparisonRow name="Unlimited Vendor Applications" col2={true} col3={true} />
-          <ComparisonRow name="Unlimited Attendees" col2={true} col3={true} isGreen={true}/>
-          <ComparisonRow name="24/7 Customer Support" col1={true} col2={true} col3={true} />
-          <ComparisonRow name="Event Page Analytics" col3={true} isGreen={true}/>
-          <ComparisonRow name="Unlimited SMS blasts" col1={true} col2={true} col3={true} />
+          <ComparisonRow name="Unlimited Tickets" col1={true} col2={true} col3={true} isGreen={true}/>
+          <ComparisonRow name="# of Admin Accounts" col1Text="1" col2Text="5" col3Text="Unlimited" /> {/*Text instead of check*/}
+          <ComparisonRow name="Unlimited Vendors" col1={true} col2={true} col3={true} isGreen={true}/>
+          <ComparisonRow name="Embedded Checkout" col2={true} col3={true} />
+          <ComparisonRow name="Unlimited Special Guests" col1={true} col2={true} col3={true} isGreen={true}/>
+          <ComparisonRow name="Mobile Check-In App Beta" col2={true} col3={true} />
+          <ComparisonRow name="Transaction Fee" col1Text="4%" col2Text="2%" col3Text="2%" isGreen={true}/> {/*Text instead of check*/}
+          <ComparisonRow name="Full Vendor CRM Suite" col1={true} col2={true} col3={true} />
+          <ComparisonRow name="Venue Maps" col1={true} col2={true} col3={true} isGreen={true}/>
+          <ComparisonRow name="Dedicated URL per event" col1={true} col2={true} col3={true} />
+          <ComparisonRow name="Social Media Promotion" col2={true} col3={true} isGreen={true}/>
+          <ComparisonRow name="Event Gallery" col1={true} col2={true} col3={true} />
+          <ComparisonRow name="Unlimited Vendor Applications" col1={true} col2={true} col3={true} isGreen={true}/>
+          <ComparisonRow name="24/7 Customer Support" col1Text="Within 24 hrs" col2Text="Within an hour" col3Text="Instant" /> {/*Text instead of check*/}
+          <ComparisonRow name="Dedicated Event Manager" col3={true} isGreen={true}/>
+          <ComparisonRow name="Unlimited SMS blasts" col1={true} col2={true} col3={true}  />
         </div>
         
-      </main>
+      </div>
     
     
     )
