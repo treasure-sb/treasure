d6461c04bb316e4532368fa6fecfbf84d56d4354
nicfigu <nicfigu@gmail.com>
2024-07-12 14:49:29 -0400

    Past Highlights to Event Gallery

--- components/landing-page/cards/Preview.tsx
+++ components/landing-page/cards/Preview.tsx
@@ -8,7 +8,7 @@ export default function Preview() {
       <div className="h-[500px] mx-4 sm:mx-0 m-auto flex flex-col justify-between border px-8 py-10 rounded-sm bg-[#71d08c] bg-opacity-20">
         <div className="space-y-4">
           <GroupPartyIcon />
-          <h4 className="text-3xl">Preview Event Highlights</h4>
+          <h4 className="text-3xl">Event Gallery</h4>
         </div>
         <p className="text-lg">
           Discover which celebrites and icons are attending. Enter giveaways and
