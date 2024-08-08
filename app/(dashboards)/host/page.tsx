2b1d1c1a8cdbd5d5aded555219e5f1ca17f2d601
John Ventura <jpventura925@gmail.com>
2024-08-08 12:38:54 -0400

    add revenue chart and stats

--- app/(dashboards)/host/page.tsx
+++ app/(dashboards)/host/page.tsx
@@ -1,20 +1,93 @@
-import Link from "next/link";
-import { Calendar } from "lucide-react";
+import createSupabaseServerClient from "@/utils/supabase/server";
+import Revenue from "./components/charts/Revenue";
+import { USDollar } from "@/lib/utils";
+import { validateUser } from "@/lib/actions/auth";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export type RevenueQueryData = {
+  created_at: string;
+  amount_paid: number;
+  event: {
+    event_roles: {
+      user_id: string;
+      role: string;
+      status: string;
+    }[];
+  };
+};
+
+type StatCardProps = {
+  title: string;
+  description?: string;
+  content: string;
+};
+
+const StatCard = ({ title, description, content }: StatCardProps) => (
+  <Card className="h-full">
+    <CardHeader>
+      <CardTitle className="text-xl">{title}</CardTitle>
+      {description && <CardDescription>{description}</CardDescription>}
+    </CardHeader>
+    <CardContent>
+      <p className="text-lg">{content}</p>
+    </CardContent>
+  </Card>
+);
 
 export default async function Page() {
+  const {
+    data: { user },
+  } = await validateUser();
+
+  const supabase = await createSupabaseServerClient();
+  const { data } = await supabase
+    .from("orders")
+    .select(
+      "created_at, amount_paid, event:events!inner(event_roles!inner(user_id, role, status))"
+    )
+    .eq("event.event_roles.user_id", user!.id)
+    .in("event.event_roles.role", ["HOST", "COHOST", "STAFF"])
+    .eq("event.event_roles.status", "ACTIVE")
+    .returns<RevenueQueryData[]>();
+
+  const revenueData: RevenueQueryData[] = data || [];
+  const totalRevenue = revenueData.reduce(
+    (amount, item) => amount + item.amount_paid,
+    0
+  );
+
+  const hostStats: StatCardProps[] = [
+    {
+      title: "Total Revenue",
+      content: USDollar.format(totalRevenue),
+    },
+    {
+      title: "Total Events",
+      content: "2",
+    },
+    {
+      title: "Total Attendees",
+      content: "102",
+    },
+  ];
+
   return (
-    <div className="lg:grid grid-cols-5 gap-4 flex flex-col">
-      <Link
-        href={`/host/events`}
-        className="bg-primary text-black flex flex-col rounded-md p-6 lg:p-4 2xl:p-6 relative group h-44 hover:bg-primary/60 transition duration-300"
-      >
-        <div className="flex lg:flex-col-reverse 2xl:flex-row justify-between">
-          <h3 className="font-semibold text-2xl lg:text-lg 2xl:text-2xl">
-            My Events
-          </h3>
-          <Calendar className="stroke-2" size={28} />
+    <div>
+      <h1 className="font-semibold text-2xl mb-2">Host Dashboard</h1>
+      <div className="flex flex-col md:flex-row md:space-x-2">
+        <Revenue user={user!} />
+        <div className="md:h-[29rem] flex flex-col justify-between space-y-2">
+          {hostStats.map((stat, index) => (
+            <StatCard key={index} {...stat} />
+          ))}
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
