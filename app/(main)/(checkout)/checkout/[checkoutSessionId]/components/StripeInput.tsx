c8dc61676cc71f450d1353e8745a88a15887f2b1
John Ventura <jpventura925@gmail.com>
2024-08-16 18:24:29 -0400

    add light mode to checkout

--- app/(main)/(checkout)/checkout/[checkoutSessionId]/components/StripeInput.tsx
+++ app/(main)/(checkout)/checkout/[checkoutSessionId]/components/StripeInput.tsx
@@ -4,7 +4,7 @@ import { InputProps } from "@/components/ui/input";
 export default function StripeInput(props: InputProps) {
   return (
     <Input
-      className="border-[1px] rounded-[5px] p-2 bg-[#0c0a09] placeholder:text-[#808080] placeholder:text-sm border-[#28211e] focus-visible:border-primary/30"
+      className="shadow-[0px_2px_4px_rgba(0,0,0,0.5),0px_1px_6px_rgba(0,0,0,0.2)] border-[1px] rounded-[5px] p-3 bg-[#fafaf5] dark:bg-[#0c0a09] placeholder:text-[#808080] placeholder:text-sm border-[#f1f1e5] dark:border-[#28211e] focus-visible:border-primary/30"
       {...props}
     />
   );
