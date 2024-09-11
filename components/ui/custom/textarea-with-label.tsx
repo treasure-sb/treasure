c5d9bb24a1ebd3cb88bbfddf9d9583365ec039a9
John Ventura <jpventura925@gmail.com>
2024-09-11 18:53:34 -0400

    fix textarea

--- components/ui/custom/textarea-with-label.tsx
+++ components/ui/custom/textarea-with-label.tsx
@@ -3,20 +3,27 @@ import * as React from "react";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "../textarea";
 import { TextareaProps } from "../textarea";
+import { cn } from "@/lib/utils";
 
-type LabelTextareaProps = TextareaProps & { label: string };
+type LabelTextareaProps = TextareaProps & {
+  label: string;
+  labelClassName?: string;
+  inputClassName?: string;
+};
 
 export function TextareaWithLabel({
   label,
+  labelClassName,
+  inputClassName,
   error,
   ...props
 }: LabelTextareaProps) {
   return (
     <div className="grid w-full gap-2">
-      <Label htmlFor={props.id} className="text-[13px]">
+      <Label htmlFor={props.id} className={cn("text-[13px]", labelClassName)}>
         {label}
       </Label>
-      <Textarea {...props} error={error} />
+      <Textarea className={cn(inputClassName)} {...props} error={error} />
     </div>
   );
 }
