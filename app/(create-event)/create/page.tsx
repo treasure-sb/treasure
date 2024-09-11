b8a51cf72f16dba9988f8262b9bedf92173e14c2
John Ventura <jpventura925@gmail.com>
2024-09-11 18:58:50 -0400

    suspense added:

--- app/(create-event)/create/page.tsx
+++ app/(create-event)/create/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import CreateEventForm from "./components/CreateEventForm";
 
 export default function Page() {
-  return <CreateEventForm />;
+  return (
+    <Suspense>
+      <CreateEventForm />
+    </Suspense>
+  );
 }
