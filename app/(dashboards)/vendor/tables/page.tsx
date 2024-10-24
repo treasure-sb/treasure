04b41ed012426eb448ad362605d11115c83616c4
santilopez10 <santisoccer10@gmail.com>
2024-10-24 15:53:40 -0400

    improved info / dialogue

--- app/(dashboards)/vendor/tables/page.tsx
+++ app/(dashboards)/vendor/tables/page.tsx
@@ -19,6 +19,7 @@ import { Button } from "@/components/ui/button";
 import EventCard from "@/components/events/shared/EventCard";
 import { convertToStandardTime } from "@/lib/utils";
 import { cn } from "@/lib/utils";
+import TableCard from "./component/TableCard";
 
 type EventWithDatesAndVendorInfo = Tables<"event_vendors"> & {
   event: EventWithDates & {
@@ -39,12 +40,14 @@ type EventDisplayVendor = {
 const LabeledText = ({
   label,
   children,
+  className,
 }: {
   label: string;
   children: React.ReactNode;
+  className?: string;
 }) => {
   return (
-    <p className="text-sm">
+    <p className={cn("text-sm", className)}>
       <span className="dark:text-background font-semibold">{label}:</span>{" "}
       {children}
     </p>
@@ -163,133 +166,14 @@ export default async function Page() {
             generalInfo,
             colorInfo,
           }) => (
-            <Dialog>
-              <DialogTrigger asChild>
-                <div
-                  key={eventDisplay.id}
-                  className="hover:translate-y-[-.5rem] hover:cursor-pointer transition duration-500 relative border flex flex-col rounded-lg p-6 gap-4"
-                >
-                  <div className="flex gap-4">
-                    {applicationInfo.application_status === "ACCEPTED" ? (
-                      <h2
-                        className={cn(
-                          "p-2 text-sm rounded-md",
-                          colorInfo.paymentStatusBG
-                        )}
-                      >
-                        {applicationInfo.payment_status}
-                      </h2>
-                    ) : null}
-                    <h2
-                      className={cn(
-                        "p-2 text-sm rounded-md",
-                        colorInfo.applicationStatusBG
-                      )}
-                    >
-                      {applicationInfo.application_status}
-                    </h2>
-                  </div>
-                  <EventCard
-                    clickable={false}
-                    user={user}
-                    showLikeButton={false}
-                    event={eventDisplay}
-                    showTicketIcon={false}
-                  />
-
-                  <div
-                    key={tableInfo.section_name}
-                    className="flex items-center gap-4"
-                  >
-                    <div className="flex space-x-2">
-                      <TicketIcon className="stroke-1 text-primary" size={24} />
-                      <p>{tableInfo.section_name}</p>
-                      <p>x{applicationInfo.table_quantity}</p>
-                    </div>
-                    <div className="flex space-x-2">
-                      <Users2Icon size={24} className="text-primary stroke-1" />
-                      <p>x{applicationInfo.vendors_at_table}</p>
-                    </div>
-                  </div>
-                  <div className="dark:bg-foreground dark:text-background border-foreground dark:border-none">
-                    <h5 className="text-md font-semibold mb-2">
-                      Vendor Check-In Information:
-                    </h5>
-                    <div className="space-y-1">
-                      <LabeledText label="Location">
-                        {generalInfo?.check_in_location || "N/A"}
-                      </LabeledText>
-                      <LabeledText label="Check In Time">
-                        {generalInfo?.check_in_time
-                          ? convertToStandardTime(generalInfo?.check_in_time)
-                          : "N/A"}
-                      </LabeledText>
-                      <LabeledText label="Wifi Availability">
-                        {generalInfo?.wifi_availability ? "Yes" : "No"}
-                      </LabeledText>
-                      <LabeledText label="Additional Information">
-                        {generalInfo?.additional_information || "None"}
-                      </LabeledText>
-                    </div>
-                  </div>
-                </div>
-              </DialogTrigger>
-              {applicationInfo.application_status === "ACCEPTED" &&
-                applicationInfo.payment_status === "PAID" && (
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>Table Confirmation</DialogTitle>
-                      <DialogDescription>
-                        Show this confirmation to the event organizer to claim
-                        your table.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <div className="space-y-2">
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Name: </p>
-                        <p>
-                          {profile!.first_name} {profile!.last_name}
-                        </p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Business Name: </p>
-                        <p>{profile!.business_name || "N/A"}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Table Section: </p>
-                        <p>{tableInfo.section_name}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Table Quantity: </p>
-                        <p>{applicationInfo.table_quantity}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Vendors at Table: </p>
-                        <p>{applicationInfo.vendors_at_table}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Inventory: </p>
-                        <p>{applicationInfo.inventory}</p>
-                      </div>
-                    </div>
-                    <div className="flex w-full justify-end">
-                      <Button
-                        asChild
-                        variant="outline"
-                        className="flex gap-1 group hover:bg-primary"
-                      >
-                        <Link href={`/events/${eventDisplay.cleaned_name}`}>
-                          <span>Go to Event</span>
-                          <ArrowUpRight
-                            size={20}
-                            className="group-hover:translate-x-[0.10rem] group-hover:-translate-y-[0.10rem] transition duration-300"
-                          />
-                        </Link>
-                      </Button>
-                    </div>
-                  </DialogContent>
-                )}
-            </Dialog>
+            <TableCard
+              eventDisplay={eventDisplay}
+              tableInfo={tableInfo}
+              applicationInfo={applicationInfo}
+              generalInfo={generalInfo}
+              colorInfo={colorInfo}
+              profile={profile}
+            />
           )
         )}
       </div>
@@ -303,133 +187,14 @@ export default async function Page() {
             generalInfo,
             colorInfo,
           }) => (
-            <Dialog>
-              <DialogTrigger asChild>
-                <div
-                  key={eventDisplay.id}
-                  className="hover:translate-y-[-.5rem] hover:cursor-pointer transition duration-500 relative border flex flex-col rounded-lg p-6 gap-4"
-                >
-                  <div className="flex gap-4">
-                    {applicationInfo.application_status === "ACCEPTED" ? (
-                      <h2
-                        className={cn(
-                          "p-2 text-sm rounded-md",
-                          colorInfo.paymentStatusBG
-                        )}
-                      >
-                        {applicationInfo.payment_status}
-                      </h2>
-                    ) : null}
-                    <h2
-                      className={cn(
-                        "p-2 text-sm rounded-md",
-                        colorInfo.applicationStatusBG
-                      )}
-                    >
-                      {applicationInfo.application_status}
-                    </h2>
-                  </div>
-                  <EventCard
-                    clickable={false}
-                    user={user}
-                    showLikeButton={false}
-                    event={eventDisplay}
-                    showTicketIcon={false}
-                  />
-
-                  <div
-                    key={tableInfo.section_name}
-                    className="flex items-center gap-4"
-                  >
-                    <div className="flex space-x-2">
-                      <TicketIcon className="stroke-1 text-primary" size={24} />
-                      <p>{tableInfo.section_name}</p>
-                      <p>x{applicationInfo.table_quantity}</p>
-                    </div>
-                    <div className="flex space-x-2">
-                      <Users2Icon size={24} className="text-primary stroke-1" />
-                      <p>x{applicationInfo.vendors_at_table}</p>
-                    </div>
-                  </div>
-                  <div className="dark:bg-foreground dark:text-background border-foreground dark:border-none">
-                    <h5 className="text-md font-semibold mb-2">
-                      Vendor Check-In Information:
-                    </h5>
-                    <div className="space-y-1">
-                      <LabeledText label="Location">
-                        {generalInfo?.check_in_location || "N/A"}
-                      </LabeledText>
-                      <LabeledText label="Check In Time">
-                        {generalInfo?.check_in_time
-                          ? convertToStandardTime(generalInfo?.check_in_time)
-                          : "N/A"}
-                      </LabeledText>
-                      <LabeledText label="Wifi Availability">
-                        {generalInfo?.wifi_availability ? "Yes" : "No"}
-                      </LabeledText>
-                      <LabeledText label="Additional Information">
-                        {generalInfo?.additional_information || "None"}
-                      </LabeledText>
-                    </div>
-                  </div>
-                </div>
-              </DialogTrigger>
-              {applicationInfo.application_status === "ACCEPTED" &&
-                applicationInfo.payment_status === "PAID" && (
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>Table Confirmation</DialogTitle>
-                      <DialogDescription>
-                        Show this confirmation to the event organizer to claim
-                        your table.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <div className="space-y-2">
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Name: </p>
-                        <p>
-                          {profile!.first_name} {profile!.last_name}
-                        </p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Business Name: </p>
-                        <p>{profile!.business_name || "N/A"}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Table Section: </p>
-                        <p>{tableInfo.section_name}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Table Quantity: </p>
-                        <p>{applicationInfo.table_quantity}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Vendors at Table: </p>
-                        <p>{applicationInfo.vendors_at_table}</p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <p className="font-semibold">Inventory: </p>
-                        <p>{applicationInfo.inventory}</p>
-                      </div>
-                    </div>
-                    <div className="flex w-full justify-end">
-                      <Button
-                        asChild
-                        variant="outline"
-                        className="flex gap-1 group hover:bg-primary"
-                      >
-                        <Link href={`/events/${eventDisplay.cleaned_name}`}>
-                          <span>Go to Event</span>
-                          <ArrowUpRight
-                            size={20}
-                            className="group-hover:translate-x-[0.10rem] group-hover:-translate-y-[0.10rem] transition duration-300"
-                          />
-                        </Link>
-                      </Button>
-                    </div>
-                  </DialogContent>
-                )}
-            </Dialog>
+            <TableCard
+              eventDisplay={eventDisplay}
+              tableInfo={tableInfo}
+              applicationInfo={applicationInfo}
+              generalInfo={generalInfo}
+              colorInfo={colorInfo}
+              profile={profile}
+            />
           )
         )}
       </div>
